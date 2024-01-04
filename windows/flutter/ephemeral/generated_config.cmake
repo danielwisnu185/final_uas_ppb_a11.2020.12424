0fa565ef2f0de36a9c2c470f3d655f023b8dc2fc
danielwisnu185 <155123931+danielwisnu185@users.noreply.github.com>
2024-01-05 00:15:21 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\kumpulan tugas\\latihan\\Shoes-Shopping-App-In-Flutter-master" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=D:\\kumpulan tugas\\latihan\\Shoes-Shopping-App-In-Flutter-master"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\kumpulan tugas\\latihan\\Shoes-Shopping-App-In-Flutter-master\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\kumpulan tugas\\latihan\\Shoes-Shopping-App-In-Flutter-master"
+  "FLUTTER_TARGET=D:\\kumpulan tugas\\latihan\\Shoes-Shopping-App-In-Flutter-master\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC83NjdkOGM3NWU4OTgwOTFiOTI1NTE5ODAzODMwZmMyNzIxNjU4ZDA3Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\kumpulan tugas\\latihan\\Shoes-Shopping-App-In-Flutter-master\\.dart_tool\\package_config.json"
+)
